c93780eccb6fd6e99f0245d344d5935675277404
deivid127 <deivid123.leite@gmail.com>
2025-07-18 16:36:37 -0300

    Add decorator example with string validation
    Introduces a decorator function that checks if arguments are strings before calling the decorated function. Demonstrates usage by decorating a string inversion function and handling type errors.

--- aula105.py
+++ aula105.py
@@ -1,2 +1,28 @@
-Funções decoradoras e decoradoras
-Decorar = Adicionar / Remover / Restringir /
+# Funções decoradoras e decoradoras
+# Decorar = Adicionar / Remover / Restringir / Alterar
+# Funções decoradoras são funções que decoram outras funções
+# Decoradores são usados para fazer o Python
+# Usar as funções deocradoras em outras funções.
+
+def criar_funcao(func):
+    def interna(*args, **kwargs):
+        for arg in args:
+            e_string(arg)
+        resultado = func(*args, **kwargs)
+        # 
+        return resultado
+    return interna
+
+def inverte_string(string):
+    return string[::-1]
+
+
+def e_string(param):
+    if not isinstance(param, str):
+        raise TypeError("param deve ser uma string")
+
+
+
+inverte_string_checando_parametro = criar_funcao(inverte_string)
+invertida = inverte_string_checando_parametro(123)
+print(invertida)
