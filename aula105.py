7caac6c363b279a5d0d5741d88d92b67a643de43
deivid127 <deivid123.leite@gmail.com>
2025-07-28 19:11:37 -0300

    Refactor decorator usage and add parameterized decorator example
    Updated aula105.py to use decorator syntax directly and improved output for decorated function. Added aula106.py to demonstrate decorators with parameters and multiple decorator stacking.

--- aula105.py
+++ aula105.py
@@ -3,6 +3,7 @@
 # Funções decoradoras são funções que decoram outras funções
 # Decoradores são usados para fazer o Python
 # Usar as funções deocradoras em outras funções.
+# Decoradores são "Syntax Sugar" (Açúcar sintático)
 
 def criar_funcao(func):
     def interna(*args, **kwargs):
@@ -10,10 +11,13 @@ def criar_funcao(func):
         for arg in args:
             e_string(arg)
         resultado = func(*args, **kwargs)
+        print(f'O seu resultado foi {resultado}')
         print('Ok, agora você foi decorada')
         return resultado
     return interna
 
+
+@criar_funcao
 def inverte_string(string):
     return string[::-1]
 
@@ -24,6 +28,5 @@ def e_string(param):
 
 
 
-inverte_string_checando_parametro = criar_funcao(inverte_string)
-invertida = inverte_string_checando_parametro('123')
+invertida = inverte_string('123')
 print(invertida)
